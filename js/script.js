87fd214ab3d95c25d4249af0e347376dcad8b2f6
Otto Kafka <ottokafka@yahoo.com>
2017-02-17 00:04:40 +0800

    Initial commit Rough Draft but fully functioning needs more css

---
+++ js/script.js
@@ -0,0 +1,80 @@
+/**
+ * Created by automac on 2/16/17.
+ */
+function playOne(){
+    document.getElementById("one").play();
+}
+
+function playTwo(){
+    document.getElementById("two").play();
+}
+
+function playThree(){
+    document.getElementById("three").play();
+}
+
+function playFour(){
+    document.getElementById("four").play();
+}
+function playFive(){
+    document.getElementById("five").play();
+}
+
+function playSix(){
+    document.getElementById("six").play();
+}
+
+function playSeven(){
+    document.getElementById("seven").play();
+}
+
+function playEight(){
+    document.getElementById("eight").play();
+}
+
+function playNine(){
+    document.getElementById("nine").play();
+}
+function playTen(){
+    document.getElementById("ten").play();
+}
+
+//------------------20 and up-----------
+function play20(){
+    document.getElementById("20").play();
+}
+
+function play30(){
+    document.getElementById("30").play();
+}
+
+function play40(){
+    document.getElementById("40").play();
+}
+function play50(){
+    document.getElementById("50").play();
+}
+
+function play60(){
+    document.getElementById("60").play();
+}
+
+function play70(){
+    document.getElementById("70").play();
+}
+
+function play80(){
+    document.getElementById("80").play();
+}
+
+function play90(){
+    document.getElementById("90").play();
+}
+function play100(){
+    document.getElementById("100").play();
+}
+
+
+
+
+
